7f16d57b0b4dca19494d1b78d3219493d846b061
TDoll <tamaranxu@hotmail.com>
2020-05-21 17:56:35 -0400

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,6 +8,7 @@ csvpath = r"C:\Users\tamar\OneDrive\Documents\GitHub\python-challenge\PyPoll\Res
 # lists to store data
 voter_id = []
 candidates = []
+unique_candidates = []
 
 # Open the CSV
 with open(csvpath) as csvfile:
@@ -21,6 +22,14 @@ with open(csvpath) as csvfile:
         
         # add voter id column to list
         voter_id.append(row[0])
+        
+        # add candidate column to list
+        candidates.append(row[2])
+        
+        # check to see if candidate name already exists in unique list
+        if row[2] not in unique_candidates:
+            
+            # add value to unique list
+            unique_candidates.append(row[2])
     
-# calculate total number of votes cast; length of list
-total_votes = len(voter_id)
+print(unique_candidates)
